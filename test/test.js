ed449e2900db9ae57dc36df893ba963da5a55059
Spencer Snyder <sasnyde2@gmail.com>
2020-06-02 00:33:56 -0400

    fix: tests + remove remark

--- test/test.js
+++ test/test.js
@@ -1,27 +1,7 @@
 const test = require('ava');
 
-const Script = require('..');
+const monitorFields = require('..');
 
-test.beforeEach(t => {
-  const script = new Script({});
-  Object.assign(t.context, { script });
-});
-
-test('returns itself', t => {
-  t.true(t.context.script instanceof Script);
-});
-
-test('sets a config object', t => {
-  const script = new Script(false);
-  t.true(script instanceof Script);
-});
-
-test('renders name', t => {
-  const { script } = t.context;
-  t.is(script.renderName(), 'script');
-});
-
-test('sets a default name', t => {
-  const { script } = t.context;
-  t.is(script._name, 'script');
+test('exports a function', t => {
+  t.true(typeof monitorFields === 'function');
 });
